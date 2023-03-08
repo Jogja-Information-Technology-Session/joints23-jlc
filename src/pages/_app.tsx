0948268fe300d7fd17ab69847347a6d965fbba17
whyspaceee <mohammad.abdan.syakura@mail.ugm.ac.id>
2023-03-08 08:50:59 +0700

    Added fonts and prettier tailwind plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { type AppType } from "next/app";
-
+import { Montserrat } from "next/font/google";
 import { api } from "~/utils/api";
+
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main className={montserrat.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default api.withTRPC(MyApp);
+  
